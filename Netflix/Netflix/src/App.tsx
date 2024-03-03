1940a3d2fdd8f25d5691e83df31467ab56c78e5f
AngeKano <Angekano002@gmail.com>
2024-03-03 10:02:46 +0000

    VV 3/3/24

---
+++ Netflix/Netflix/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <a href="">qsqsxcxcqqs</a>
+    </>
+  );
+}
+
+export default App;
